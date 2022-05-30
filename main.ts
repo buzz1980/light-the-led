ae602ad4b5846980a9aca52ad8b37ceb1cd1d903
buzz1980 <105562851+buzz1980@users.noreply.github.com>
2022-05-30 20:23:12 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
